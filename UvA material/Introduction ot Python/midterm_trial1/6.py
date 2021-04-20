765c7b3f2d1a31cff114f2876ee82266ab83eeb8
JINHYEON KIM <78076248+Jaykim1234@users.noreply.github.com>
2021-04-20 14:00:07 +0200

    Update 6.py

--- UvA material/Introduction ot Python/midterm_trial1/6.py
+++ UvA material/Introduction ot Python/midterm_trial1/6.py
@@ -9,11 +9,26 @@ then it should return the dictionary:
 {1: 'some', 2: 'extra', 3: 'words'}
 """
 
-#dict = {1: 'some', 2: 'extralong', 3: 'words'}
-for i in dict:
-    if len(dict[i]) >= 5:
-        dict[i] = dict[i][0:5]
-    else:
-        pass
-print(dict)
+#dict를 변수명으로 하니까 오류가 난다!!
 
+
+#dict = {1: 'some', 2: 'extralong', 3: 'words'}
+# for i in dict:
+#     if len(dict[i]) >= 5:
+#         dict[i] = dict[i][0:5]
+#     else:
+#         pass
+# print(dict)
+
+
+# items는 까먹었었는데 다시 기억하자!!
+def main(diction):
+    for i,v in diction.items():
+
+        if len(diction[i]) >= 5:
+            diction[i] = diction[i][0:5]
+        else:
+            pass
+    return diction
+
+# dict를 update하는 방법도 있다!!
