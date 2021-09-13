bb8af57bbfacdc25707d995a32a992c917f6a31b
Jaykim1234 <kjin19930000@gmail.com>
2021-09-13 22:52:32 +0200

    .

--- 코딩테스트/programmers/DFS & BFS/네트워크.py
+++ 코딩테스트/programmers/DFS & BFS/네트워크.py
@@ -27,12 +27,25 @@ image1.png
 
 def solution(n, computers):
     answer = 0
-    visited = [False for i in range(n)]
+    visited = [False for i in range(n)] # False False False  ~~
     for com in range(n):
         if visited[com] == False:
             BFS(n, computers, com, visited)
             answer += 1
     return answer
+
+def BFS(n, computers, com, visited):
+    visited[com] = True
+    queue = []
+    queue.append(com)
+    while len(queue) != 0:
+        com = queue.pop(0)
+        visited[com] = True
+        for connect in range(n):
+            if connect != com and computers[com][connect] == 1:
+                if visited[connect] ==  False:
+                    queue.append(connect)
+
 
 
 
