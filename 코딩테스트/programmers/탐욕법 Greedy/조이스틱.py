3b77b5d6644ddc4a415ee6c797bf8173d8843489
JINHYEON KIM <78076248+Jaykim1234@users.noreply.github.com>
2021-11-25 20:00:37 +0100

    Update 조이스틱.py

---
+++ 코딩테스트/programmers/탐욕법 Greedy/조이스틱.py
@@ -0,0 +1,99 @@
+"""
+조이스틱
+문제 설명
+조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다.
+ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
+
+조이스틱을 각 방향으로 움직이면 아래와 같습니다.
+
+▲ - 다음 알파벳
+▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
+◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
+▶ - 커서를 오른쪽으로 이동
+예를 들어 아래의 방법으로 "JAZ"를 만들 수 있습니다.
+
+- 첫 번째 위치에서 조이스틱을 위로 9번 조작하여 J를 완성합니다.
+- 조이스틱을 왼쪽으로 1번 조작하여 커서를 마지막 문자 위치로 이동시킵니다.
+- 마지막 위치에서 조이스틱을 아래로 1번 조작하여 Z를 완성합니다.
+따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다.
+만들고자 하는 이름 name이 매개변수로 주어질 때, 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
+
+제한 사항
+name은 알파벳 대문자로만 이루어져 있습니다.
+name의 길이는 1 이상 20 이하입니다.
+입출력 예
+name	return
+"JEROEN"	56
+"JAN"	    23
+"""
+# 위 아래 방향 찾기
+# 좌 우 최적 값 찾기
+
+"AERAEA"
+6
+1245
+['a','a'].index('b')
+
+def r_left(string, idx):
+    count = 0
+    directions = [-1, 1]
+ 
+    if 'A' not in string:
+        return True
+        
+    for direction in directions:
+        while string[idx] != 'A':
+            idx += direction
+            count += 1
+            if idx == len(string) :
+                idx = 0
+            elif idx <0 :
+                idx = len(string)
+        count = min(count, 100**1000)\
+    
+    return count
+
+r_left("JAN", 0)
+
+#    current_loc = [0 if string[i] != 'A' else 1 for i in range(len(string)) ]
+    
+    # idx_lst = [idx for idx in range(len(string)) if string[idx] != 'A' ]
+    # for idx_1 in range(len(idx_lst)):
+    #     if idx_1 + 1 != len(idx_lst):
+    #         if (idx_lst[idx_1+1] - idx_lst[idx_1]) < len(string)
+    #     else:
+    #         if idx`` 
+
+
+
+def updown(idx, string):
+    count = 0
+    if (ord(string[idx]) - ord('A')) < (ord('Z')- ord(string[idx])):
+        count += ord(string[idx]) - ord('A')
+    else:
+        count += ord('Z')- ord(string[idx]) + 1
+    return count
+
+
+
+
+
+
+
+
+
+
+
+       
+
+location = [0 if i == 'A' else 1 for i in string]
+
+def solution(string):
+
+string = "JEROEN"
+[0 if i == 'A' else '1' for i in string]
+
+
+print(ord('J')- ord('A'))
+print(ord('N')- ord('A'))
+print(ord('Z')- ord('N'))
