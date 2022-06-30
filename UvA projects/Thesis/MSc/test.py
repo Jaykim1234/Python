231e00ee5d388e09d15c9a11e87251db6e9fd6e4
JINHYEON KIM <78076248+Jaykim1234@users.noreply.github.com>
2022-06-30 22:19:25 +0200

    thesis

--- UvA projects/Thesis/MSc/test.py
+++ UvA projects/Thesis/MSc/test.py
@@ -1,5 +1,5 @@
-import asc.operation    # calcpkg 패키지의 operation 모듈을 가져옴
-import asc.geometry     # calcpkg 패키지의 geometry 모듈을 가져옴
+import sys
 
-print(asc.operation.add(10, 20))  
-print(asc.operation.mul(10, 20)) 
+from ascm.operation import add, mul
+
+print(add(10,20))
