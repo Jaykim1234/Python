b957931f3e39d8d0a7f03e91f80a9a19cfa39811
JINHYEON KIM <78076248+Jaykim1234@users.noreply.github.com>
2022-08-23 23:59:32 +0200

    Update 뱀.cpp

--- 코딩테스트/이것이 코딩테스트다/cpp/그리디/뱀.cpp
+++ 코딩테스트/이것이 코딩테스트다/cpp/그리디/뱀.cpp
@@ -1,11 +1,27 @@
-
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#define APPLE 1
+#define SNAKE 2
 using namespace std;
-  
-// Main() function: where the execution of program begins
-int main()
-{
-    // prints hello world
-    cout << "Hello World";
-  
-    return 0;
+
+int dy[] = {0, 1, 0 , -1};
+int dx[] = {1, 0, -1, 0};
+
+vector<vector<int>> map;
+
+queue<pair<int, char>> snakeDir;
+
+int n;
+
+int getDir(int dir, char roDir) {
+    if (roDir == 'L') dir = (dir -1 < 0 ? 3: dir -1);
+    else dir = (dir +1>3 ? 0: dir +1);
+    return dir;
+}
+
+int move() {
+    deque<pair<int, int>> snake;
+    snake.push_back(make_pair())
 }
